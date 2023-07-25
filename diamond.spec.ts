7eff416499aaba614e6c2f4aa12c1bacff2f310a
Marco Emrich <marco.emrich@codecentric.de>
2023-07-25 16:58:03 +0200

    prepare diamond kata

---
+++ diamond.spec.ts
@@ -0,0 +1,21 @@
+import { it, describe, expect } from "vitest";
+
+// A => A
+
+// B =>
+// .A.
+// B.B
+// .A.
+
+// C =>
+// ..A..
+// .B.B.
+// C...C
+// .B.B.
+// ..A..
+
+describe("Name", () => {
+  it("should ", () => {
+    expect(1).toEqual(1);
+  });
+});
