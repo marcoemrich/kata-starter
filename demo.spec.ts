e5309dfd64f7c6c328fb9d5c3310100232e305b8
Marco Emrich <marco.emrich@codecentric.de>
2023-12-06 04:26:00 +0100

    cleanup as TDD starter

--- demo.spec.ts
+++ demo.spec.ts
@@ -1,19 +1,5 @@
 import { it, describe, expect } from "vitest";
 
-// A => A
-
-// B =>
-// .A.
-// B.B
-// .A.
-
-// C =>
-// ..A..
-// .B.B.
-// C...C
-// .B.B.
-// ..A..
-
 describe("Name", () => {
   it("should ", () => {
     expect(1).toEqual(1);
